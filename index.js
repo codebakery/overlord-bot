3511aae77a24883dcc9008a43290497cb1cc3427
Dmitry Gladkov <dmitry.gladkov@gmail.com>
2016-12-01 17:52:14 +0200

    WIP

--- index.js
+++ index.js
@@ -1,28 +1,76 @@
-var restify = require('restify');
-var builder = require('botbuilder');
+const restify = require('restify');
+const botbuilder = require('botbuilder');
+const fetch = require('node-fetch');
 
-//=========================================================
-// Bot Setup
-//=========================================================
+const config = Object.assign({
+  'HOST': '127.0.0.1',
+  'PORT': 3978,
+  'MESSAGING_ENDPOINT': '/api/messages',
+}, require('./config.json'));
 
-// Setup Restify Server
-var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+const server = restify.createServer({
+  name: process.env.npm_package_name,
 });
-  
-// Create chat bot
-var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
-});
-var bot = new builder.UniversalBot(connector);
-server.post('/api/messages', connector.listen());
 
-//=========================================================
-// Bots Dialogs
-//=========================================================
-
-bot.dialog('/', function (session) {
-    session.send("Hello World");
+server.listen(config.PORT, config.HOST, () => {
+  console.log('%s listening to %s', server.name, server.url); 
 });
+
+const connector = new botbuilder.ChatConnector({
+  appId: config.MICROSOFT_APP_ID,
+  appPassword: config.MICROSOFT_APP_PASSWORD,
+});
+
+const bot = new botbuilder.UniversalBot(connector);
+server.post(config.MESSAGING_ENDPOINT, connector.listen());
+
+const currentAddress = null;
+
+
+const availableCommands = {
+    save: (session) => {
+      currentAddress = session.message.address;
+      session.send('Route address saved successfully.'); // <at id="28:868e0687-2b3e-43c0-a415-9af02a444772">Overlord</at> text
+    },
+    hi: (session) => {
+      const firstName = session.message.user.name.split(' ')[0]
+      session.send(`Hello, ${firstName}.`);
+    },
+    norris: (session) => {
+      session.send('A fact about Chuck Norris:');
+      fetch('http://api.icndb.com/jokes/random')
+        .then(function(response) {
+          return response.json();
+        }).then(function(data) {
+          if (data.type === 'success') {
+            session.send(data.value.joke);
+          }
+        });
+    },
+    '(mooning)': (session) => {
+      session.send('(finger)');
+    }
+};
+
+var stripss = /<ss type=".+">(.+)<\/ss>/g;
+function prepareMessage(text) {
+  return '<ss type=".+">(.+)</ss>'.replace(stripss, '$1');
+}
+
+bot.dialog('/',
+  (session) => {
+    let messageText;
+    if (session.message.address.conversation.isGroup) {
+      messageText = session.message.text.slice(63); // 
+    } else {
+      messageText = session.message.text;
+    }
+    messageText = prepareMessage(prepareMessage);
+    if (availableCommands.hasOwnProperty(messageText)) {
+      availableCommands[messageText](session);
+    } else {
+      session.send(`Available commands:<br/>${Object.keys(availableCommands).join('<br/>')}`);
+    }
+  }
+);
+
